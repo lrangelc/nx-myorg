1a163ea2ffd4467aac3a7882b03408f30fe84b56
Luis Rangel Castro <luisrangelc@rpaipos.com>
2020-10-06 14:06:21 -0600

    add api

--- apps/todos/src/app/app.component.ts
+++ apps/todos/src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+interface Todo {
+  title: string;
+}
 
 @Component({
   selector: 'myorg-root',
@@ -7,4 +11,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todos';
+  todos: Todo[] = [];
+  // todos: Todo[] = [{ title: 'Todo 1' }, { title: 'Todo 2' }];
+
+  constructor(private http: HttpClient) {
+    this.fetch();
+  }
+
+  fetch() {
+    this.http.get<Todo[]>('/api/todos').subscribe((t) => (this.todos = t));
+  }
+
+  addTodo() {
+    this.todos.push({
+      title: `New todo ${Math.floor(Math.random() * 1000)}`,
+    });
+
+    this.http.post('/api/addTodo', {}).subscribe(() => {
+      this.fetch();
+    });
+  }
 }
